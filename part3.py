853820a985356c8a8509b5bc35948b023bc9b7c6
David-bz <davidbzach@gmail.com>
2021-07-08 02:07:06 +0300

    experiments 1-4

--- part3.py
+++ part3.py
@@ -263,12 +263,12 @@ class Part_3:
 
 
 if __name__ == '__main__':
-    p = Part_3(in_features, num_classes, data_mode='reg')
-    p.run_hw4_experiment(name='exp1: regular train set', description="Regular setup")
-    p = Part_3(in_features, num_classes, data_mode='random_train')
-    p.run_hw4_experiment(name='exp2: train randomized', description="Randomized train set")
-    p = Part_3(in_features, num_classes, data_mode='half_random')
-    p.run_hw4_experiment(name='exp3: half randomized', description="Half of train set randomized")
+    # p = Part_3(in_features, num_classes, data_mode='reg')
+    # p.run_hw4_experiment(name='exp1: regular train set', description="Regular setup")
+    # p = Part_3(in_features, num_classes, data_mode='random_train')
+    # p.run_hw4_experiment(name='exp2: train randomized', description="Randomized train set")
+    # p = Part_3(in_features, num_classes, data_mode='half_random')
+    # p.run_hw4_experiment(name='exp3: half randomized', description="Half of train set randomized")
     p = Part_3(in_features, num_classes, data_mode='adversarial')
     p.run_hw4_experiment(name='exp4: manipulate adversarially', description="Half of train set manipulated")
     # f_exe =[p.perform_grid_search,
