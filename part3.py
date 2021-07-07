a1b688f93681fd3f266daabb7edb8d225385f622
David-bz <davidbzach@gmail.com>
2021-07-08 02:32:25 +0300

    patch

--- part3.py
+++ part3.py
@@ -273,7 +273,7 @@ if __name__ == '__main__':
     # p = Part_3(in_features, num_classes, data_mode='half_random')
     # p.run_hw4_experiment(name='exp3: half randomized', description="Half of train set randomized")
     p = Part_3(in_features, num_classes, data_mode='adversarial')
-    p.run_hw4_experiment(name='exp4:_manipulate_adversarially', description="Half_of_train_set_manipulated")
+    p.run_hw4_experiment(name='exp4_manipulate_adversarially', description="Half_of_train_set_manipulated")
     # f_exe =[p.perform_grid_search,
     #         p.set_optimization_experiment,
     #         p.set_initialization_experiment,
